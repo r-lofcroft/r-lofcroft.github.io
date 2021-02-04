b20dee71313b889faea5b9aa6880437407ea4ad3
Robin Williscroft <robin.williscroft@gmail.com>
2021-02-04 16:09:05 +0100

    first commit

---
+++ precache-manifest.96a5cd1658af993014ea935f2e05fc86.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3d19941bfe22bc1acb0",
+    "url": "/css/app.ecaf70ce.css"
+  },
+  {
+    "revision": "a838c93a333cd944ab9566585dd5be72",
+    "url": "/img/logo.a838c93a.png"
+  },
+  {
+    "revision": "2a06a97328aebab74dc33c0534252fb9",
+    "url": "/img/shield-fill-check.2a06a973.svg"
+  },
+  {
+    "revision": "92d105e634b994e62808ec31657a85b4",
+    "url": "/img/shield-fill-x.92d105e6.svg"
+  },
+  {
+    "revision": "d5073da16f277c3b8436732d2d28c893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3d19941bfe22bc1acb0",
+    "url": "/js/app.26a94a24.js"
+  },
+  {
+    "revision": "39e992e1381ab8a5fe25",
+    "url": "/js/chunk-vendors.590c2232.js"
+  },
+  {
+    "revision": "27a30e9a98bef9e1d276d4aff6e75e27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
